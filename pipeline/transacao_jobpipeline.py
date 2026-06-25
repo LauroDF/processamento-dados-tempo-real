131c4144f65bace5bcd4b5b4dc248c7d7c69bcea
Gabriel C Mascarenhas <gabrielcmascarenhas@gmail.com>
2026-06-25 18:31:08 -0300

    Adiciona readme explicando as imagens da etapa 2 e retira comentários do código

--- pipeline/transacao_jobpipeline.py
+++ pipeline/transacao_jobpipeline.py
@@ -5,7 +5,6 @@ from pyflink.table import StreamTableEnvironment
 from pyflink.datastream.functions import MapFunction
 from pyflink.common.typeinfo import Types
 
-# --- 1. A NOSSA REGRA PERSONALIZADA EM PYTHON ---
 class SalvarNoRedis(MapFunction):
     def __init__(self, host, port):
         self.host = host
@@ -13,14 +12,9 @@ class SalvarNoRedis(MapFunction):
         self.cliente_redis = None
 
     def open(self, runtime_context):
-        # Este método roda 1 vez quando o operário (TaskManager) liga.
-        # Ele abre a conexão com o seu container do Valkey.
-        self.cliente_redis = redis.Redis(host=self.host, port=self.port, decode_responses=True)
+        self.redis_client = redis.Redis(host=self.host, port=self.port, decode_responses=True)
 
     def map(self, linha):
-        # Este método roda para CADA transação que passar no filtro (>500)
-        # 'linha' contém os dados do Kafka na ordem: (id, cliente, valor, categoria, timestamp)
-        
         id_transacao = str(linha[0])
         
         dados = {
@@ -31,17 +25,15 @@ class SalvarNoRedis(MapFunction):
             "timestamp": str(linha[4])
         }
 
-        self.cliente_redis.hset(f"fraude:{id_transacao}", mapping=dados)
-        
+        self.redis_client.hset(f"fraude:{id_transacao}", mapping=dados)
         return f"Registro fraude:{id_transacao}"
 
 def kafka_pipeline():
     env = StreamExecutionEnvironment.get_execution_environment()
     env.set_parallelism(1)
-    
+
     t_env = StreamTableEnvironment.create(stream_execution_environment=env)     
 
-    # Lemos do Kafka usando o conector oficial
     t_env.execute_sql("""
     CREATE TABLE kafka_source (
      `id_transacao` STRING,
@@ -58,17 +50,10 @@ def kafka_pipeline():
     'format' = 'json'
     )""")
 
-    # 1. Fazemos a query SQL para filtrar apenas compras grandes
-    tabela_filtrada = t_env.sql_query("SELECT * FROM kafka_source WHERE valor_total > 500.0")
-
-    # 2. Convertemos a Tabela SQL para um DataStream (Fluxo de Dados contínuo)
-    fluxo_dados = t_env.to_data_stream(tabela_filtrada)
-
-    # 3. Jogamos o fluxo para a nossa função Python (que vai disparar os dados para o Valkey)
-    fluxo_dados.map(SalvarNoRedis(host='valkey', port=6379), output_type=Types.STRING())
-
-    # 4. Damos o start no motor do Flink!
-    env.execute("Pipeline_Kafka_Para_Valkey")
+    filtered_table = t_env.sql_query("SELECT * FROM kafka_source WHERE valor_total > 500.0")
+    data_stream = t_env.to_data_stream(filtered_table)
+    data_stream.map(SalvarNoRedis(host='valkey', port=6379), output_type=Types.STRING())
+    env.execute("Pipeline_Kafka_for_Valkey")
 
 if __name__ == '__main__':    
     kafka_pipeline()
---
+++ prints/etapa_2/README.md
@@ -0,0 +1,13 @@
+# Imagens do Flink e do Valkey
+
+## 1ª imagem: job rodando no flink
+
+![1](1.png)
+
+## 2ª imagem: redisinsight-ui
+
+![2](2.png)
+
+## 3ª imagem: dados chegando no redis insight-ui
+
+![3](3.png)
